7eb1a6b081cee81836fec676491ebec4e31b9cdd
Ian G. <iangaunt999@gmail.com>
2024-11-14 08:37:30 -0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-mod testing;
-mod tutorial { include!("tut/tutorial.rs"); }
-mod structs { include!("tut/structs.rs");}
-mod practice { include!("practice/codewars.rs"); }
-
 fn main() {
-    testing::test();
-    tutorial::tutorial();
-    structs::make_shapes();
-    
-    practice::test_questions();
+    println!("Hello, world!");
 }
